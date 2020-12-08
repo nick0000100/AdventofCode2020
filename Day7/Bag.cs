a9e8f6ebe09a7be022d70004301ccf77507d9751
Nicholas Sor <v-nisor@microsoft.com>
2020-12-08 15:40:53 -0800

    day 7 solution

--- Day7/Bag.cs
+++ Day7/Bag.cs
@@ -1,7 +1,27 @@
 using System.Collections.Generic;
+using System.Linq;
 public class Bag
 {
-    public string BagColor;
-    public string[] TempContents;
-    public Dictionary<Bag, int> BagContents = new Dictionary<Bag, int>();
+    public string Color;
+    public string[] RawContents;
+    public Dictionary<Bag, int> InnerBags = new Dictionary<Bag, int>();
+
+    public Bag(string Color, string[] RawContents)
+    {
+        this.Color = Color;
+        this.RawContents = RawContents;
+    }
+
+    //Populates the bag with the inner bags
+    public void FillBag(List<Bag> AllBags)
+    {
+        for(int i = 1; i < this.RawContents.Length; i++)
+        {
+            if(!this.RawContents[i].Contains("no other"))
+            {
+                InnerBags.Add(AllBags.Where(x => x.Color == RawContents[i].Substring(2)).FirstOrDefault(), int.Parse(RawContents[i].Substring(0, 1)));
+            }
+        }
+        
+    }
 }
--- Day7/Program.cs
+++ Day7/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Day7
 {
@@ -7,7 +8,18 @@ namespace Day7
     {
         static void Main(string[] args)
         {
-            ReadInput();
+            HashSet<Bag> BagsWithGold = new HashSet<Bag>();
+            List<Bag> AllBags = FillBags(ReadInput());
+            foreach(Bag Bag in AllBags)
+            {
+                if(!BagsWithGold.Contains(Bag))
+                {
+                    CheckShinyGold(Bag, BagsWithGold);
+                }
+            }
+            System.Console.WriteLine(BagsWithGold.Count);
+            Bag Goldie = AllBags.Where(x => x.Color == "shiny gold").FirstOrDefault();
+            System.Console.WriteLine(GetInnards(Goldie, 1));
         }
         public static List<string> ReadInput()
         {
@@ -19,11 +31,66 @@ namespace Day7
             while((line = file.ReadLine()) != null)  
             {  
                 Input.Add(line);
-                System.Console.WriteLine(line);
-
             }
             file.Close();
             return Input;
+        }
+
+        // Checks if bags contain a "Shiny Gold" Bag.
+        public static bool CheckShinyGold(Bag Bag, HashSet<Bag> BagsWithGold)
+        {
+            bool HasShinyGold = false;
+            if(Bag.Color == "shiny gold")
+            {
+                return true;
+            }
+            if(Bag.InnerBags.Count == 0)
+            {
+                return false;
+            }
+            foreach(KeyValuePair<Bag, int> CurrentBag in Bag.InnerBags)
+            {
+                if(CheckShinyGold(CurrentBag.Key, BagsWithGold))
+                {
+                    BagsWithGold.Add(Bag);
+                    HasShinyGold = true;
+                }
+            }
+            return HasShinyGold;
+        }
+
+        //Gets the number of bags inside a specified colored bag
+        public static int GetInnards(Bag Bag, int NumberOfBags)
+        {
+            int Innards = 0;
+            if(Bag.InnerBags.Count == 0)
+            {
+                return 0;
+            }
+            foreach(KeyValuePair<Bag, int> CurrentBag in Bag.InnerBags)
+            {
+                Innards += GetInnards(CurrentBag.Key, CurrentBag.Value * NumberOfBags) + CurrentBag.Value * NumberOfBags;
+            }
+            return Innards;
+        }
+
+        //Gets all of the bags from input.
+        public static List<Bag> FillBags(List<string> Input)
+        {
+            List<Bag> AllBags = new List<Bag>();
+            foreach(string Line in Input)
+            {
+                string[] InnerBags = Line.Split(new string[] {" contain ", ", ", ".", " bags", " bag"}, StringSplitOptions.RemoveEmptyEntries);
+                if(!AllBags.Contains(AllBags.Where(x => x.Color == InnerBags[0]).FirstOrDefault()))
+                {
+                    AllBags.Add(new Bag(InnerBags[0], InnerBags));
+                }
+            }
+            foreach(Bag Bag in AllBags)
+            {
+                Bag.FillBag(AllBags);
+            }
+            return AllBags;
         }
     }
 }
