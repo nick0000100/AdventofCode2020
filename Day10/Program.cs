135e3bbcb94d9a2651fa4799097513b1e3a4c08a
Nicholas Sor <v-nisor@microsoft.com>
2020-12-11 11:52:43 -0800

    Day 10

---
+++ Day10/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace day10
+{
+    class Program
+    {
+        static Dictionary<int, long> Permutations = new Dictionary<int, long>();
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> VoltageDifferences = CountOfAdapters(ReadInput());
+            System.Console.WriteLine(VoltageDifferences["One"] * VoltageDifferences["Three"]);
+            System.Console.WriteLine(GetPermutations(ReadInput(), 0));
+        }
+
+        public static List<int> ReadInput()
+        {
+            string Line;  
+            List<int> Input = new List<int>();
+            
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file = new System.IO.StreamReader(@"./Input.txt");  
+            while((Line = file.ReadLine()) != null)  
+            {  
+                Input.Add(Int32.Parse(Line));
+            }
+            file.Close();
+            Input.Sort();
+            Input.Insert(0, 0);
+            return Input;
+        }
+
+        public static Dictionary<string, int> CountOfAdapters(List<int> Input)
+        {
+            Input.Add(Input[Input.Count - 1] + 3);
+            Dictionary<string, int> Adapters = new Dictionary<string, int>()
+            {
+                {"One", 0},
+                {"Two", 0},
+                {"Three", 0}
+            };
+            for(int i = 0; i < Input.Count - 1; i++)
+            {
+                int Difference = Input[i + 1] - Input[i];
+                if(Difference == 1)
+                {
+                    Adapters["One"]++;
+                }
+                else if(Difference == 2)
+                {
+                    Adapters["Two"]++;
+                }
+                else if(Difference == 3)
+                {
+                    Adapters["Three"]++;
+                }
+            }
+            return Adapters;
+        }
+
+        public static long GetPermutations(List<int> Input, int Position)
+        {
+            int NextPostition = Position + 1;
+            // Checking if the current number has been checked already
+            if(Permutations.ContainsKey(Position))
+            {
+                return Permutations[Position];
+            }
+            long Total = 0;
+            if(Position == Input.Count - 1)
+            {
+                return 1;
+            }
+            while((NextPostition < Input.Count) && (Input[NextPostition] - Input[Position] <= 3))
+            {
+                Total += GetPermutations(Input, NextPostition);
+                NextPostition += 1;
+            }
+            // Adds current number to cache
+            Permutations[Position] = Total;
+            return Permutations[Position];
+        }
+    }
+}
---
+++ Day9/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace day9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<long> Input = ReadInput();
+            System.Console.WriteLine(FirstIncorrect(ReadInput(), 25));
+            System.Console.WriteLine(FindSum(Input, FirstIncorrect(Input, 25)));
+
+        }
+
+        public static List<long> ReadInput()
+        {
+            string line;  
+            List<long> Input = new List<long>();
+            
+            // Read the file and display it line by line.  
+            System.IO.StreamReader file = new System.IO.StreamReader(@"./Input.txt");  
+            while((line = file.ReadLine()) != null)  
+            {  
+                Input.Add(long.Parse(line));
+            }
+            file.Close();
+            return Input;
+        }
+        public static long FirstIncorrect(List<long> Input, int CountOfNumbersToCheck)
+        {
+            int CurrentPosition = CountOfNumbersToCheck;
+            long CurrentNumber = Input[CurrentPosition];
+            var CurrentList = Input.Skip(CountOfNumbersToCheck - CurrentPosition).Take(CountOfNumbersToCheck).OrderBy(x => x).ToList();
+            while(IncorrectChecker(CurrentList, CurrentNumber))
+            {
+                IncorrectChecker(CurrentList, CurrentNumber);
+                CurrentPosition++;
+                CurrentNumber = Input[CurrentPosition];
+                CurrentList = Input.Skip(CurrentPosition - CountOfNumbersToCheck).Take(CountOfNumbersToCheck).OrderBy(x => x).ToList();
+            }
+            CurrentNumber = Input[CurrentPosition];
+            return CurrentNumber;
+        }
+
+        public static long FindSum(List<long> Input, long IncorrectNumber)
+        {
+            int LowerBound = 0;
+            int UpperBound = 1;
+            List<long> Range = new List<long>()
+            {
+                Input[LowerBound],
+                Input[UpperBound]
+            };
+            long Sum = Range.Sum();
+            while(Sum != IncorrectNumber)
+            {
+                if(Sum > IncorrectNumber)
+                {
+                    LowerBound++;
+                }
+                else
+                {
+                    UpperBound++;
+                }
+                Range = Input.GetRange(LowerBound, UpperBound - LowerBound);
+            }
+            return Range.Min() + Range.Max();
+        }
+
+        private static bool IncorrectChecker(List<long> CurrentList, long CurrentNumber)
+        {
+            foreach(int ListNumber in CurrentList)
+            {
+                long Difference = CurrentNumber - ListNumber;
+                if(CurrentList.Exists(x => x == Difference))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
